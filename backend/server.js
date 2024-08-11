83f4bff370cf8b174a7c7d18588fb917bbf47c42
Abhishek solanki <abhishek@Abhisheks-MacBook-Air.local>
2024-08-12 03:40:23 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,19 +10,21 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 import { app, server } from "./socket/socket.js";
-
+const __dirname = path.resolve();
 dotenv.config();
 app.use(express.json()); //to parse incoming info with json payloads
 app.use(cookieParser());
 const PORT = process.env.PORT || 5001; // Use process.env.PORT if available, otherwise default to 5001
 
-app.get("/", function (req, res) {
-  res.send("Hello guys");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
